a79088a58bd8d4dc96ff6e4abfcf81c73152b59e
Yogesh Jajoria <yogeshjajoria2019@gmail.com>
2024-04-14 09:21:39 +0530

    now saves private key directly into the server folder

--- gen_key.py
+++ gen_key.py
@@ -4,7 +4,7 @@ from cryptography.fernet import Fernet
 (public, private) = rsa.newkeys(512)
 
 open("public.pem", "wb").write(public.save_pkcs1("PEM"))
-open("private.pem", "wb").write(private.save_pkcs1("PEM"))
+open("./server/private.pem", "wb").write(private.save_pkcs1("PEM"))
 
 # fernet keys
 fernet_key = Fernet.generate_key()
